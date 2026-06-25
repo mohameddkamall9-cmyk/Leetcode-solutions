f8c2eb6ff810b891e1b795763def0f25f3624d27
mohamed mohamed <mohameddkamall9@gmail.com>
2026-06-25 19:05:28 +0800

    added comments to explaim the algorithm I used

--- Hard/medianOfAttays/bruteForceSolution.java
+++ Hard/medianOfAttays/bruteForceSolution.java
@@ -1,18 +1,22 @@
 package Hard.medianOfAttays;
 import java.util.Arrays;
+// This is a brute force solution to find the median of two sorted arrays.
 
 public class bruteForceSolution {
 
     public static void main(String[] args) {
+        // Example input arrays
         int[] nums1 = {1, 3};
         int[] nums2 = {2, 4, 6, 8};
-        
+        // Call the solution method to find the median
         solution(nums1, nums2);
     }
 
     public static void solution(int[] nums1, int[] nums2) {
+       
         int n1 = nums1.length;
         int n2 = nums2.length;
+        // create a new array to hold the merged elements of nums1 and nums2
         int k = 0;
         
         int[] mergedArray = new int[n1 + n2];
@@ -28,7 +32,7 @@ public class bruteForceSolution {
         Arrays.sort(mergedArray);
 
         int total = mergedArray.length;
-        
+        // Calculate the median based on whether the total number of elements is odd or even
         if (total % 2 == 1) {
             System.out.println("Median is: " + (double) mergedArray[total / 2]);
         } else {
